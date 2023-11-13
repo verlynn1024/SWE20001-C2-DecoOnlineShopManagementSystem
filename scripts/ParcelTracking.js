f4588ad957e18eccc91e8d33db47a8509195e86b
Jaanane Catherine Mahadevan <113246343+jaycee7903@users.noreply.github.com>
2023-11-14 00:10:13 +0800

    Add files via upload

---
+++ scripts/ParcelTracking.js
@@ -0,0 +1,9 @@
+function redirectToHome(){
+    console.log("redirect to home page");
+    window.location.href = '1_1_Home.html';
+}
+
+function redirectToLiveTraking(){
+    console.log("redirect to location tracking");
+    window.location.href = '7_1_Tracking_Location.html';
+}
