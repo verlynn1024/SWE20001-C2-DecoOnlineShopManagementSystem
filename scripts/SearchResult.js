087be3b32ec5299c692bdc0544c32ba45c90fa4e
Jaanane Catherine Mahadevan <113246343+jaycee7903@users.noreply.github.com>
2023-11-20 08:35:29 +0800

    Add files via upload

---
+++ scripts/SearchResult.js
@@ -0,0 +1,15 @@
+
+function redirectToHome(){
+    console.log("redirect to home page");
+    window.location.href = '1_1_Home.html';
+}
+
+function redirectToLogin(){
+    console.log("redirect to login in page");
+    window.location.href = '5_1_Login___1 .html';
+}
+
+function redirectToBag(){
+    console.log("redirect to bag item page");
+    window.location.href = '3_3_Bag_detail.html';
+}
