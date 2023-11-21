3a0b0e08cbc10d1df5bdc13450e7171c84dda087
Jaanane Catherine Mahadevan <113246343+jaycee7903@users.noreply.github.com>
2023-11-21 13:18:53 +0800

    Add files via upload

---
+++ scripts/MeetUs.js
@@ -0,0 +1,46 @@
+function redirectToHome(){
+    console.log("redirect to home page");
+    window.location.href = '1_1_Home.html';
+}
+
+function redirectToSofas(){
+    console.log("redirect to Sofa page");
+    window.location.href = '8_2_Product___Sofas.html';
+}
+
+function redirectToChairs(){
+    console.log("redirect to Chair page");
+    window.location.href = '8_3_Products__Chairs.html';
+}
+
+function redirectToBeds(){
+    console.log("redirect to bed page");
+    window.location.href = '8_4__Products__Bed.html';
+}
+
+function redirectToLights(){
+    console.log("redirect to Lights page");
+    window.location.href = '8_5_Products__Lighting.html';
+}
+
+function redirectToLogin(){
+    console.log("redirect to login in page");
+    window.location.href = '5_1_Login___1 .html';
+}
+
+function redirectToBag(){
+    console.log("redirect to bag item page");
+    window.location.href = '3_3_Bag_detail.html';
+}
+
+function redirectToHomeOffice(){
+    console.log("redirect to home office page");
+    window.location.href = '9_1_Category__Home_Office.html';
+}
+
+function redirectToMeetUs(){
+    console.log("redirect to meet us page");
+    window.location.href = '9_0_Meet_Us.html';
+}
+
+
